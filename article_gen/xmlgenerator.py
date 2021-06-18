02dbf929d8a250531dc95372578745a594fd8322
ramu <ummadishettyramu@gmail.com>
2021-06-19 01:36:09 +0530

    pep8 standards and few modifications

--- article_gen/xmlgenerator.py
+++ article_gen/xmlgenerator.py
@@ -1,10 +1,14 @@
-import json, pandas, random
-from hashlib import sha1
-from jinja2 import Environment, FileSystemLoader
+import json
+import random
 import string
 from datetime import datetime
+from hashlib import sha1
 
-tewiki = '''<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="te">
+import pandas
+import numpy as np
+from jinja2 import Environment, FileSystemLoader
+
+tewiki = """<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="te">
 	<siteinfo>
 		<sitename>tewiki</sitename>
 		<dbname>indicwiki</dbname>
@@ -38,132 +42,139 @@ tewiki = '''<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:x
 			<namespace key="2303" case="case-sensitive">Gadget definition talk</namespace>
 			<namespace key="2600" case="first-letter">Topic</namespace>
 		</namespaces>
-	</siteinfo>'''
+	</siteinfo>"""
 
 page_id = 400001
-user_id = '61' #not assigned to me yet - need to change this (?)
-username = 'Shreyateru'#change this also
+user_id = "61"  # not assigned to me yet - need to change this (?)
+username = "Shreyateru"  # change this also
+
 
 def sha36(page_id):
-	page_id = str(page_id).encode('utf-8')
-	sha16 =sha1(page_id).hexdigest()
-	sha10 =int(sha16, 16)
+    page_id = str(page_id).encode("utf-8")
+    sha16 = sha1(page_id).hexdigest()
+    sha10 = int(sha16, 16)
 
-	chars =[]
-	alphabets = string.digits +string.ascii_lowercase
-	while sha10>0:
-		sha10, r = divmod(sha10, 36)
-		chars.append(alphabets[r])
-	
-	return ''.join(reversed(chars)) 
+    chars = []
+    alphabets = string.digits + string.ascii_lowercase
+    while sha10 > 0:
+        sha10, r = divmod(sha10, 36)
+        chars.append(alphabets[r])
+
+    return "".join(reversed(chars))
+
 
 def addPage(title, wikiText):
-	global user_id, username
-	pglen = len(wikiText)
-	title = title.replace('&','అండ్')
-	time =datetime.now().strftime("%Y-%m-%dT%H-%M-%SZ")
-	page = '''\n\n
+    global user_id, username
+    pglen = len(wikiText)
+    title = title.replace("&", "అండ్")
+    time = datetime.now().strftime("%Y-%m-%dT%H-%M-%SZ")
+    page = ( """\n\n
 	<page>
-		<title>'''+title+'''</title>
+		<title>""" + title + """</title>
 		<ns>0</ns>
-		<id>'''+str(page_id)+'''</id>
+		<id>""" + str(page_id) + """</id>
 		<revision>
-			<id>'''+str(page_id)+'''</id>
-			<timestamp>'''+time+'''</timestamp>
+			<id>""" + str(page_id) + """</id>
+			<timestamp>""" + time + """</timestamp>
 			<contributor>
-				<username>'''+username+'''</username>
-				<id>'''+str(user_id)+'''</id>
+				<username>""" + username + """</username>
+				<id>""" + str(user_id) + '''</id>
 			</contributor>
 			<comment>xmlpagecreated</comment>
 			<model>wikitext</model>
 			<format>text/x-wiki</format>
-			<text xml:space ="preserve" bytes="'''+str(pglen)+'''">
-			\n'''+wikiText+'''
+			<text xml:space ="preserve" bytes="''' + str(pglen) + """">
+			\n""" + wikiText + """
 			</text>
 		</revision>
 	</page>
-	\n\n'''
+	\n\n"""
+    )
 
-	return page
+    return page
 
-def xmlGenerator(titles,textTemplate, dataFrame):
-	codes = dataFrame['Code'].tolist()
-	#indices = [6,17,31,210,782,134,104]
-	#codes= []
-	#for i in indices:
-	#	codes.append(dataFrame.loc[i]['Code'])
-	#codes = random.sample(codes,4)
-	#codes = codes +['C-2014-0009']
-	#print(len(codes))
-	word_counts = []
-	pages = ""
-	titles_list = []
-	global page_id
-	for code in codes:
-		details = dataFrame[dataFrame['Code']==code].to_dict(orient='records')[0]
-		for col in details:
-			if type(details[col]) == str:
-				details[col] = details[col].replace('&','అండ్')
-			else:
-				for i in range(len(details[col])):
-					details[col][i] = details[col][i].replace('&','అండ్')
-		#title = titles[code]
-		title = titles[code].strip()
-		print('title',title, code)
-		titles_list.append(title)
-		wikiText = textTemplate.render(details)
-		word_counts.append(len(wikiText.split(' ')))
-		pages+=addPage(title, wikiText)
-		page_id+=1
-	#print(page_id)
-	print('Len :',len(set(titles)))
-	min_count  = min(word_counts)
-	max_count = max(word_counts)
-	avg_count = sum(word_counts)/len(word_counts)
-	#print('MIN : '+str(min_count))
-	#print('MAX : '+str(max_count))
-	#print('AVG : '+str(avg_count))
-	import numpy as np
-	np.save('word_counts.npy',word_counts)
-	return pages
+
+def xmlGenerator(titles, textTemplate, dataFrame):
+    codes = dataFrame["Code"].tolist()
+    # indices = [6,17,31,210,782,134,104]
+    # codes= []
+    # for i in indices:
+    # 	codes.append(dataFrame.loc[i]['Code'])
+    # codes = random.sample(codes,4)
+    # codes = codes +['C-2014-0009']
+    # print(len(codes))
+    word_counts = []
+    pages = ""
+    titles_list = []
+    global page_id
+    for code in codes:
+        details = dataFrame[dataFrame["Code"] == code].to_dict(orient="records")[0]
+        for col in details:
+            if type(details[col]) == str:
+                details[col] = details[col].replace("&", "అండ్")
+            else:
+                for i in range(len(details[col])):
+                    details[col][i] = details[col][i].replace("&", "అండ్")
+        # title = titles[code]
+        title = titles[code].strip()
+        print("title", title, code)
+        titles_list.append(title)
+        wikiText = textTemplate.render(details)
+        word_counts.append(len(wikiText.split(" ")))
+        pages += addPage(title, wikiText)
+        page_id += 1
+    # print(page_id)
+    print("Len :", len(set(titles)))
+    min_count = min(word_counts)
+    max_count = max(word_counts)
+    avg_count = sum(word_counts) / len(word_counts)
+    # print('MIN : '+str(min_count))
+    # print('MAX : '+str(max_count))
+    # print('AVG : '+str(avg_count))
+    
+
+    np.save("word_counts.npy", word_counts)
+    return pages
+
 
 def main():
-	file_loader = FileSystemLoader('templates')
-	env = Environment(loader=file_loader)
-	pages=""
-	titlefile = './new_titles'
+    file_loader = FileSystemLoader("templates")
+    env = Environment(loader=file_loader)
+    pages = ""
+    titlefile = "./other_files/new_titles"
 
-	titles = {}
-	list_titles = []
-	with open(titlefile,'r') as tf:
-		# csvreader = csv.reader(tf)
-		# fields = next(csvreader)
-		# print(len(fields))
-		lines = tf.readlines()
-		for l in lines:
-			y = l.split(' ')
-			t = ' '.join(y[1:-1])
-			titles[y[-1].strip()] = t
-			print(y[-1].strip())
-			list_titles.append(t)
-	print(len(set(list_titles)))
-	#exit()
-	print('new')
-	domains = ['1135']
-	for subdomain in domains:
-		xmlTextTemplate = env.get_template('telugu_hospitals.j2')
-		subdomainfile = 'translations/'+subdomain+'_hosp_telugu.json'
-		data = pandas.read_json(subdomainfile)
-		pages+=xmlGenerator(titles,xmlTextTemplate, data)
-	
+    titles = {}
+    list_titles = []
+    with open(titlefile, "r") as tf:
+        # csvreader = csv.reader(tf)
+        # fields = next(csvreader)
+        # print(len(fields))
+        lines = tf.readlines()
+        for l in lines:
+            y = l.split(" ")
+            t = " ".join(y[1:-1])
+            titles[y[-1].strip()] = t
+            print(y[-1].strip())
+            list_titles.append(t)
+    print(len(set(list_titles)))
+    # exit()
+    print("new")
+    domains = ["1135"]
+    for subdomain in domains:
+        xmlTextTemplate = env.get_template("telugu_hospitals.j2")
+        subdomainfile = "translations/" + subdomain + "_hosp_telugu.json"
+        data = pandas.read_json(subdomainfile)
+        pages += xmlGenerator(titles, xmlTextTemplate, data)
 
-	#save pages
-	xmlpage = tewiki+pages+'</mediawiki>'
-	#xmlpage = xmlpage.replace('&','అండ్')
-	xmlpage = xmlpage.replace('మరియు','అండ్')
-	xmlpage = xmlpage.replace('యొక్క','')
-	xmlpage = xmlpage.replace('జాతీయగీతము','ఎక్స్-రే')
-	f = open('1135_hosp_tel.xml','w')
-	f.write(xmlpage)
-	f.close()
+    # save pages
+    xmlpage = tewiki + pages + "</mediawiki>"
+    # xmlpage = xmlpage.replace('&','అండ్')
+    xmlpage = xmlpage.replace("మరియు", "అండ్")
+    xmlpage = xmlpage.replace("యొక్క", "")
+    xmlpage = xmlpage.replace("జాతీయగీతము", "ఎక్స్-రే")
+    f = open("1135_hosp_tel.xml", "w")
+    f.write(xmlpage)
+    f.close()
+
+
 main()
